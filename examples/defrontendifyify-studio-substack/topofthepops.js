9d2a8458b9621eb7ded92417f7825e1e4a182be3
James Kyburz <james.kyburz@gmail.com>
2014-11-16 16:19:14 +0100

    trumpet refactor

--- examples/defrontendifyify-studio-substack/topofthepops.js
+++ examples/defrontendifyify-studio-substack/topofthepops.js
@@ -1,14 +1,14 @@
 var request = require('hyperquest');
-var concat = require('concat-stream');
-var select = require('html-select');
-var tokenize = require('html-tokenize');
 var baseurl = 'http://studio.substack.net';
 var baudio = require('baudio');
+var trumpet = require('trumpet');
 var vm = require('vm');
 
-var s = select('.row > .link', link);
+var tr = trumpet();
 
-request(baseurl + '/-/recent').pipe(tokenize()).pipe(s);
+tr.selectAll('.row > .link', link);
+
+request(baseurl + '/-/recent').pipe(tr);
 
 var links = [];
 
@@ -16,20 +16,18 @@ function link(e) {
   links.push(e.getAttribute('href'));
 }
 
-s.resume();
 player();
 
 function player() {
   var href = links.shift();
   if (!href) return setTimeout(player, 1000);
-  var s = select('#code', function(e) {
-    e.createReadStream().on('data', function (row) {
-      if (row[0] === 'text') play(row[1].toString());
-    });
+  var tr = trumpet();
+  tr.select('#code', function(e) {
+    e.createReadStream().on('data', play);
   });
   if (href) {
     console.log('processing %s', href);
-    request(baseurl + href).pipe(tokenize()).pipe(s);
+    request(baseurl + href).pipe(tr);
   }
 }
 
